54a48011c81bd9d86e38098463a0771c9f2a5271
wanguiwaweru <wanguiwawerub@gmail.com>
2022-11-16 23:15:43 +0300

    model

--- model.py
+++ model.py
@@ -3,12 +3,12 @@ from typing import Optional
 
 class SentimentRequest(BaseModel):
     text: Optional[str]
-    request_id: Optional[str]
+    request_id: Optional[str]    
     class Config:
         arbitrary_types_allowed = True
         schema_extra = {
             "example":{
-                "text": "the real crap is that annoying thing", 
+                "text": "Your are annoying.", 
                 "request_id": "1234-04x4-894"       
             }
         }
