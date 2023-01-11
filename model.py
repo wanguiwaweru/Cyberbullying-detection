651ece02c49c7e014a6f1aea67b23fbf241a5060
Wangui Waweru <74382189+wanguiwaweru@users.noreply.github.com>
2023-01-11 16:11:28 +0300

    updated sentiment response model

--- model.py
+++ model.py
@@ -1,18 +1,25 @@
 from pydantic import BaseModel
 from typing import Optional
+from typing import TypedDict
+
 
 class SentimentRequest(BaseModel):
     text: Optional[str]
-    request_id: Optional[str]    
+    request_id: Optional[str]
+
     class Config:
         arbitrary_types_allowed = True
         schema_extra = {
-            "example":{
-                "text": "Your are annoying.", 
-                "request_id": "1234-04x4-894"       
+            "example": {
+                "text": "You are annoying.",
+                "request_id": "1234-04x4-894"
             }
         }
+
+
 class SentimentResponse(BaseModel):
     status_code: Optional[int]
     message: Optional[str]
-    sentiment: Optional[list[str]]
+    post: Optional[str]
+    confidence: Optional[dict]
+    sentiment: Optional[list]
