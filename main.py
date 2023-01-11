9b0c351e90438165deda7711472eab5b783c7f36
wanguiwaweru <wanguiwawerub@gmail.com>
2023-01-11 09:30:46 +0300

    include model's confidence in each sentiment predicted

--- main.py
+++ main.py
@@ -1,49 +1,57 @@
+import json
 import torch
 import numpy as np
 from fastapi import FastAPI
 from model import SentimentRequest,SentimentResponse
 from transformers import AutoModelForSequenceClassification, AutoTokenizer
 
-model = AutoModelForSequenceClassification.from_pretrained('./model_save')
+prediction_model = AutoModelForSequenceClassification.from_pretrained('./model_save')
 tokenizer = AutoTokenizer.from_pretrained('./model_save')
 
 labels = ['Cyberbullying', 'Insult', 'Profanity', 'Sarcasm', 'Threat', 'Exclusion', 'Pornography', 'Spam']
 id2label = {idx:label for idx, label in enumerate(labels)}
 label2id = {label:idx for idx, label in enumerate(labels)}
-#"""scp -r \Users\Sunshine\Desktop\env\nlp NLPServer@20.116.58.131:/home/NLPServer/Nlp"""
+
 app = FastAPI()
 
 @app.get("/")
 async def root():
     docs_path = 'http://127.0.0.1:8000/docs'
     return f"Hello welcome to this sentiment classifier. Visit {docs_path} to try out."  
+    
 # function should call model and predict tags 
 
 @app.post("/predict")
 async def predictions(request_text:SentimentRequest):
+    
     res = SentimentResponse()
-    print(request_text.text)
+    res.post = request_text.text
+    
     encoding = tokenizer(request_text.text, return_tensors="pt")
-    encoding = {k: v.to(model.device) for k,v in encoding.items()}
-    outputs = model(**encoding)
+    encoding = {k: v.to(prediction_model.device) for k,v in encoding.items()}
+    outputs = prediction_model(**encoding)
     logits = outputs.logits
     #print(logits)
+
     sigmoid = torch.nn.Sigmoid()
     probs = sigmoid(logits.squeeze().cpu())
+    proba = sigmoid(logits.squeeze().cpu().detach())
+    confidence = dict(zip(labels,proba))
+    
     predictions = np.zeros(probs.shape)
-    #print(probs)
     predictions[np.where(probs >= 0.5)] = 1
-    
     predicted_labels = [id2label[idx] for idx, label in enumerate(predictions) if label == 1.0]
-    print(predicted_labels)
     
+    res.confidence = {(k,format(float(v),".4f"))for k,v in confidence.items()}
+
+
     if len(predicted_labels) >=1:
-        res.status_code = 200
-        res.message = f'Request processed successfully. The predicted sentiments are {predicted_labels}'
+        res.message = f'Request processed successfully.'
         res.sentiment = predicted_labels
+        res.status_code = 200
     else:
-        res.status_code = 200
-        res.message = f'Request processed successfully.This post does not appear to have harmful sentiments.'
+        res.message = f'Request processed successfully. This post does not appear to have harmful sentiments.'
         res.sentiment = predicted_labels
+        res.status_code = 200
     
     return res
